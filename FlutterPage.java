5423cf8fc9894e2cc48df0795dbc385071cf3364
Harsh <unagctystudent2@gmail.com>
2020-06-20 15:18:18 -0400

    data points down

--- FlutterPage.java
+++ FlutterPage.java
@@ -1,14 +1,35 @@
 import java.util.ArrayList;
 public class FlutterPage {
   public String pageName;
-  public ArrayList<int[]> variableList;
-  public FlutterPage(pageName, variableList) {
+  public ArrayList<String[]> variableList;
+  public FlutterPage(String pageName, ArrayList<String[]> variableList) {
     this.pageName = pageName;
     this.variableList = variableList;
   }
   public String assembleString() {
     String wholeFile = "";
     wholeFile += "import 'dart:math';\nimport 'package:flutter/material.dart';\nimport 'package:flushbar/flushbar.dart';\nimport 'useful.dart';\n\n";
-    String stateful = String.format("class %s extends StatefulWidget {\n\t%s ({Key key}) : super(key: key);\n\t@override\n\t%sState createState() => %sState();\n};\n\n", pageName, pageName, pageName, pageName);
+    wholeFile += String.format("class %s extends StatefulWidget {\n\t%s ({Key key}) : super(key: key);\n\t@override\n\t%sState createState() => %sState();\n};\n\n", pageName, pageName, pageName, pageName);
+    wholeFile += String.format("class %sState extends State<%s> {\n\tGlobalKey scaffold = GlobalKey();\n", pageName, pageName);
+    for (String[] i : variableList) {
+      String defaultVal;
+      if (i[0].equals("int")) {
+        defaultVal = "0";
+      } else {
+        defaultVal = "''";
+      }
+      wholeFile += String.format("\t%s %s = %s;\n", i[0], i[1], defaultVal);
+    }
+    for (String[] i: variableList) {
+      if (i[2].equals("req")) {
+        wholeFile += String.format("\tBoolean %sRed = Boolean(false);\n", i[1]);
+      }
+    }
+    for (String[] i: variableList) {
+      if (i[3].equals("text")) {
+        wholeFile += String.format("\tTextEditingController %sController = TextEditingController();\n", i[1]);
+      }
+    }
+    return wholeFile;
   }
 }
---
+++ driver.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+public class driver {
+  public static void main(String[] args) {
+    String pageName = "Objective";
+    ArrayList<String[]> vars = new ArrayList<>();
+    addTo("String", "event", "req", "no", vars);
+    addTo("String", "team", "req", "text", vars);
+    addTo("String", "match", "req", "text", vars);
+    addTo("String", "initials", "req", "text", vars);
+    addTo("String", "autoCross", "req", "no", vars);
+    addTo("String", "autoPreload", "req", "text", vars);
+    addTo("String", "autoLow", "req", "text", vars);
+    addTo("String", "autoOuter", "req", "text", vars);
+    addTo("String", "autoInner", "req", "text", vars);
+    addTo("String", "rotationControl", "req", "no", vars);
+    addTo("String", "positionControl", "req", "no", vars);
+    addTo("int", "cellLow", "no", "no", vars);
+    addTo("int", "cellOuter", "no", "no", vars);
+    addTo("int", "cellInner", "no", "no", vars);
+    addTo("int", "cellDroppedLow", "no", "no", vars);
+    addTo("int", "cellDroppedHigh", "no", "no", vars);
+    addTo("String", "endgameClimb", "req", "no", vars);
+    addTo("String", "endgameLevel", "req", "no", vars);
+    addTo("String", "notes", "req", "text", vars);
+    FlutterPage fp = new FlutterPage(pageName, vars);
+    System.out.println(fp.assembleString());
+  }
+  public static void addTo(String type, String value, String req, String field, ArrayList<String[]> list) {
+    String[] toAdd = {type, value, req, field};
+    list.add(toAdd);
+  }
+}
