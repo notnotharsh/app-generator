db6f9d7f17aae1e0e4d63ebc8945082d4a58bd2e
Harsh <unagctystudent2@gmail.com>
2020-07-18 14:55:12 -0400

    bottom tabs and size limit

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="favicon.ico" type="image/x-icon" />
     <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@300&display=swap" rel="stylesheet">
     <link type="text/css" href="style.css" rel="stylesheet" />
@@ -14,13 +15,25 @@
       <div id="square">
         <h1>Scouting App Generator</h1>
         <div id="settings">
-          <?php
-            exec("javac java/*.java");
-            $code = shell_exec("java -cp java Main");
-            # echo $code;
-          ?>
+          <div id="bar">
+            <div id="tabs">
+            </div>
+            <div id="bottom">
+              <div id="plus">
+                <p>Add Page</p>
+              </div>
+              <div id="state">
+                <p>Save/Load State</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   </body>
 </html>
+<?php
+  exec("javac java/*.java");
+  $code = shell_exec("java -cp java Main");
+  # echo $code;
+?>
--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function pageSize() {
-  var width = innerWidth;
-  var height = innerHeight;
+  var width = document.body.clientWidth;
+  var height = document.body.clientHeight;
   if (width / height >= 7/4) {
-    document.getElementById("page").style.height = innerHeight + "px";
-    document.getElementById("page").style.width =  (7/4 * innerHeight) + "px";
+    document.getElementById("page").style.height = document.body.clientHeight + "px";
+    document.getElementById("page").style.width =  (7/4 * document.body.clientHeight) + "px";
   } else {
-    document.getElementById("page").style.width = innerWidth + "px";
-    document.getElementById("page").style.height =  (4/7 * innerWidth) + "px";
+    document.getElementById("page").style.width = document.body.clientWidth + "px";
+    document.getElementById("page").style.height =  (4/7 * document.body.clientWidth) + "px";
   }
 }
