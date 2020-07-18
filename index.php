00a6dff086928cf3997209b93b4a9da8aa123586
Harsh <unagctystudent2@gmail.com>
2020-07-18 09:57:31 -0400

    favicon and b64

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <link rel="icon" href="favicon.ico" type="image/x-icon" />
     <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@300&display=swap" rel="stylesheet">
     <link type="text/css" href="style.css" rel="stylesheet" />
     <script type="text/javascript" src="script.js"></script>
@@ -11,16 +12,15 @@
       <div id="phone">
       </div>
       <div id="square">
-        <h1>App Generator</h1>
+        <h1>Scouting App Generator</h1>
         <div id="settings">
+          <?php
+            exec("javac java/*.java");
+            $code = shell_exec("java -cp java Main");
+            echo $code;
+          ?>
         </div>
       </div>
     </div>
   </body>
 </html>
-
-<?php
-  exec("javac java/*.java");
-  $code = shell_exec("java Main");
-  // echo $code;
-?>
